d974956833651a01e17198fca582844312a79609
pikapy486 <69073509+pikapy486@users.noreply.github.com>
2020-08-01 12:53:06 +0300

    Add files via upload

---
+++ skript.js/skript.js
@@ -0,0 +1,80 @@
+//document.getElementById('input').value="1";
+
+
+function addThree() {
+	document.getElementById('input').value = document.getElementById('input').value + "3";
+}
+
+function addFour() {
+	document.getElementById('input').value = document.getElementById('input').value + "4";
+}
+
+function addFive() {
+	document.getElementById('input').value = document.getElementById('input').value + "5";
+}
+
+function addSix() {
+	document.getElementById('input').value = document.getElementById('input').value + "6";
+}
+
+function addSeven() {
+	document.getElementById('input').value = document.getElementById('input').value + "7";
+}
+
+function addEight() {
+	document.getElementById('input').value = document.getElementById('input').value + "8";
+}
+
+function addNine() {
+	document.getElementById('input').value = document.getElementById('input').value + "9";
+}
+
+function addNol() {
+	document.getElementById('input').value = document.getElementById('input').value + "0";
+}
+
+
+function addOmnowety() {
+	document.getElementById('input').value = document.getElementById('input').value + "x";
+}
+
+function addMinus() {
+	document.getElementById('input').value = document.getElementById('input').value + "-";
+}
+
+
+function addPLus() {
+	document.getElementById('input').value = document.getElementById('input').value + "+";
+}
+
+
+
+
+function addAc() {
+	document.getElementById('input').value = "";
+}
+
+function addPlusandMinus() {
+	document.getElementById('input').value = document.getElementById('input').value + "+/-";
+}
+
+
+function addProxent() {
+	document.getElementById('input').value = document.getElementById('input').value + "%";
+}
+
+
+
+
+function addSapitay() {
+	document.getElementById('input').value = document.getElementById('input').value + ".";
+}
+
+function otwet() {
+	document.getElementById('input').value = eval(document.getElementById('input').value)
+}
+
+function kalk(argument) {
+	document.getElementById("input").value = document.getElementById("input").value + argument;
+
+}
